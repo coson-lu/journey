322ac45784abf3c9b73f16d6e8734f45da166f2f
Coson Lu <cosonlu27@gmail.com>
2024-10-09 21:17:38 -0700

    added styling to password page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,8 @@ function App() {
       setAccess(true)
     }
   }
-
+  let x = (value, istart, istop, ostart, ostop) => ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
+  
   return (
     <>
       {access === true ? (
@@ -29,14 +30,25 @@ function App() {
           </Routes>
         </>
       ) : (
-        <div class='password-container'>
-          <form onSubmit={handleSubmit}>
-            <h1>Enter password for website access: </h1>
-            <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <input type='submit' placeholder='submit'/>
-          </form>
-        </div>
-        
+        <>
+          <div class='password-container'>
+            <form onSubmit={handleSubmit}>
+              <h1 id='password-prompt'>Enter password for website access: </h1>
+              <input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+              <input type='submit' placeholder='submit'/>
+            </form>
+          </div>
+          <div class='big-password-container'>
+            {Array.from({ length: (1800 - 450) }, (_, index) => {
+              const i = 1800 - index;
+              console.log(i);
+              return (
+                <div key={i} style={{ width: i + 'px', height: i + 'px', transform: 'rotate(45deg) ', backgroundColor: `rgb(${x(i, 450, 1800, 27, 18)}, ${x(i, 450, 1800, 27, 18)}, ${x(i, 450, 1800, 50, 18)})`, position: 'absolute', top: `calc(50% - (${i / 2}px))`, left: `calc(50% - (${i / 2}px))`, 'borderRadius': `${x(i, 450, 1800, 80, 300)}px`, 'zIndex': -50000}}>
+                </div>
+              ); // USE P5 MAP function!
+            })}
+          </div>
+        </>
       )}
       
     </>
